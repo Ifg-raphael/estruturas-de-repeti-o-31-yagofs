3d13d7900b6e4b90859e2e6b6cc941d29af2ac1c
Yagofs <yagofsarmento8@gmail.com>
2025-09-01 20:27:44 -0300

    Solução final

--- calcula_somatorio.py
+++ calcula_somatorio.py
@@ -1,1 +1,9 @@
-# Sua solução aqui
+"""CÓDIGO PARA CALCULAR SOMATÓRIO"""
+#Inserção de valores e atribuição de variaveis 
+n = int(input())
+soma = 0
+#Estrutura de repetição para calcular o somatório
+for i in range(2, n + 1):
+    soma += i / (i * (i - 1))
+#Saída do resultado
+print(f"{soma:.2f}")
